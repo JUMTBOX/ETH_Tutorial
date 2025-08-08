45c80fa1b97f1dec2ecad158dac2a44db71b83ee
JUMTBOX <yjey12@naver.com>
2025-08-08 20:25:12 +0900

    ...

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,18 +1,19 @@
 const { GENESIS_DATA } = require("../config");
+const { keccakHash } = require("../util/index");
 
 const HASH_LENGTH = 64;
 const MAX_HASH_VALUE = parseInt("f".repeat(HASH_LENGTH), 16);
+const MAX_NONCE_VALUE = 2 ** 64;
 
 class Block {
   constructor({ blockHeaders }) {
     this.blockHeaders = blockHeaders;
   }
-
+  /** @param {{lastBlock:Block}} param1 */
   static calculateBlockTargetHash({ lastBlock }) {
     const value = (MAX_HASH_VALUE / lastBlock.blockHeaders.difficulty).toString(
       16
     );
-
     if (value.length > HASH_LENGTH) {
       return "f".repeat(HASH_LENGTH);
     }
@@ -20,7 +21,37 @@ class Block {
     return "0".repeat(HASH_LENGTH - value.length) + value;
   }
 
-  static mineBlock({ lastBlock, beneficiary }) {}
+  /** @param {{lastBlock:Block, beneficiary: object}} param1 */
+  static mineBlock({ lastBlock, beneficiary }) {
+    const target = Block.calculateBlockTargetHash({ lastBlock });
+    let timestamp, truncatedBlockHeaders, header, nonce, underTargetHash;
+
+    do {
+      timestamp = Date.now();
+      truncatedBlockHeaders = {
+        parentHash: keccakHash(lastBlock.blockHeaders),
+        beneficiary,
+        difficulty: lastBlock.blockHeaders.difficulty + 1,
+        number: lastBlock.blockHeaders.number + 1,
+        timestamp,
+      };
+
+      header = keccakHash(truncatedBlockHeaders);
+      nonce = Math.floor(Math.random() * MAX_NONCE_VALUE);
+
+      underTargetHash = keccakHash(header + nonce);
+    } while (underTargetHash > target);
+
+    console.info(
+      "============== underTargetHash ============== \n",
+      underTargetHash
+    );
+    console.info("============== target ============== \n", target);
+
+    return new this({
+      blockHeaders: { ...truncatedBlockHeaders, nonce },
+    });
+  }
 
   static genesis() {
     return new this(GENESIS_DATA);
@@ -28,3 +59,10 @@ class Block {
 }
 
 module.exports = Block;
+
+const block = Block.mineBlock({
+  lastBlock: Block.genesis(),
+  beneficiary: "foo",
+});
+
+console.info("========= Block instanciate result ========= \n", block);
